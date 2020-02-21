7ca18c106715974351f88dd7ebb59c45d02b2aa6
Laila Campbell <lailacampbell96@gmail.com>
2020-02-21 17:47:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,10 +10,12 @@ end
 
 def long_planeteer_calls# code an argument here
   # Your code here
+array.any? { |word| word.length > 4}
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
-primary_colors.each do |color|
+
