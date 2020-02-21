d33738e227c2232c75db1c3e1c868eff948aede1
Laila Campbell <lailacampbell96@gmail.com>
2020-02-21 17:45:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(name) # code an argument here
   # Your code here
-  array.each_with_index { |word, index| puts "#{index+1} #{word}"}
-  
+ primary_colors.each do |color|
+  puts #{name}
 end
 
 def summon_captain_planet# code an argument here
@@ -16,3 +16,4 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+primary_colors.each do |color|
